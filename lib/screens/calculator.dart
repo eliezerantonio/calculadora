9a768b5488800a3f6059b74430501b7935e989a2
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-03-24 02:01:51 +0100

    tela principal

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatelessWidget {
+  const Calculator({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text("dislay"),
+          Text("keyboard"),
+        ],
+      ),
+    );
+  }
+}
