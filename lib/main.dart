37ddb6a147005a8a0513c2c5168bd4af276cc3e3
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-03-26 01:18:16 +0100

    bloqueando orientacao

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:calculadora/screens/calculator.dart';
 import 'package:flutter/material.dart';
- 
+import 'package:flutter/services.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Calculator()
-    );
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    return MaterialApp(title: 'Material App', home: Calculator());
   }
 }
