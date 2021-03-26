6881d3f9b8b7541786a00fc8a0e85fc9b0b3f2ab
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-03-26 02:03:24 +0100

    adicionar digito

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,11 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    SystemChrome.setPreferredOrientations(
+      [
+        DeviceOrientation.portraitUp,
+      ],
+    );
     return MaterialApp(title: 'Material App', home: Calculator());
   }
 }
